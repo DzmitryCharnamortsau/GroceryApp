48f178a2d768939ab0d72462055e74b369e8d50b
Dzmitry Charnamortsau <d.charnamortsau@gmail.com>
2025-12-16 10:17:04 -0800

    finished sql queries in MySqlCategoryDao

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -7,8 +7,6 @@ import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
